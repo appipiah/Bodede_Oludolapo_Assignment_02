3a66ae39386fc50a001cb78e21f0bdd26b826147
Oludolapo Bodede <bodededolapo@gmail.com>
2022-07-15 01:31:33 +0100

    Assignment one

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,24 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    let sum = 0
+
+    if (arrayOfNumbers.length == 0) {
+        return null;
+    }
+    else{
+        for (let i = 0; i < arrayOfNumbers.length; i++) {
+            sum += arrayOfNumbers[i] 
+        }
+        return sum
+    }
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -27,10 +41,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0
+
+    for (i = 0; i < arrayOfNumbers.length; i++){
+        if (arrayOfNumbers[i] % 2 == 0){
+            count++
+        }
+    }
+    return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
